d8b7ec461450eef81e334cbc7844f37d4266660c
Tu Nombre <milena.hernandezb@gmail.com>
2025-12-15 00:07:14 -0500

    deploy + svc

--- micro2/app.py
+++ micro2/app.py
@@ -1,13 +1,9 @@
-from flask import Flask, jsonify
-
+from flask import Flask
 app = Flask(__name__)
 
-@app.route("/data", methods=["GET"])
-def data():
-    return jsonify({
-        "service": "micro2",
-        "message": "respuesta desde micro2"
-    })
+@app.route("/")
+def hola():
+    return "Hola hola desde Microservicio 2"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8080)
