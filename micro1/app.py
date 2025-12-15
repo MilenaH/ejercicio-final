0aecf70694ad679b960ef0a919aecc00914f644d
Tu Nombre <milena.hernandezb@gmail.com>
2025-12-15 00:11:58 -0500

    Micros

--- micro1/app.py
+++ micro1/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hola():
-    return "Hola desde  1 Microservicio 1"
+    return "Hola desde 4 1 Microservicio 1"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
